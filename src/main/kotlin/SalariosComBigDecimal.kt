fdfb3c39a23e063893eca4d2d6502ea18e7fd77c
jpmoraess <moraesdeveloperweb@gmail.com>
2022-01-03 03:51:34 -0300

    Somatória com Reduce

--- src/main/kotlin/SalariosComBigDecimal.kt
+++ src/main/kotlin/SalariosComBigDecimal.kt
@@ -19,8 +19,24 @@ fun main() {
     }.toTypedArray()
 
     println("Salários com aumento: ${salariosComAumento.contentToString()}")
+
+    /**
+     * Calcular a somatória dos salários
+     * Calcular a somatória dos salários por 6 meses para saber os gastos...
+     */
+
+    val totalSalarios = salariosComAumento.somatoria()
+    println("Valor total dos salários com aumento: $totalSalarios")
+
+    val totalSalariosProximos6Meses = salariosComAumento.somatoria() * BigDecimal(6)
+    println("Valor total dos salários com aumento em 6 meses: $totalSalariosProximos6Meses")
+
 }
 
 fun bigDecimalArrayOf(vararg values: String): Array<BigDecimal> {
     return Array(values.size) { i -> values[i].toBigDecimal() }
+}
+
+fun Array<BigDecimal>.somatoria(): BigDecimal {
+    return this.reduce { acc, valor -> acc + valor }
 }
