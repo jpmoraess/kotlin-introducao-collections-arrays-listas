907419c404a341ea8e70fe08957f660f62644327
jpmoraess <moraesdeveloperweb@gmail.com>
2022-01-03 02:22:28 -0300

    Iterando por um array

--- src/main/kotlin/IntroArrays.kt
+++ src/main/kotlin/IntroArrays.kt
@@ -1,25 +1,22 @@
 fun main() {
-    val idade1 = 25
-    val idade2 = 19
-    val idade3 = 33
-    val idade4 = 20
+    val idades = intArrayOf(25, 19, 33, 20, 55)
 
-    val maiorIdade = if (idade1 > idade2 && idade1 > idade3 && idade1 > idade4) {
-        idade1
-    } else if (idade2 > idade3 && idade2 > idade4) {
-        idade2
-    } else if (idade3 > idade4) {
-        idade3
-    } else {
-        idade4
+    var maiorIdade = 0
+    for (idade in idades) {
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
     }
 
-    println("maior idade é: $maiorIdade")
+    println("maior idade: $maiorIdade")
 
-    val idades = IntArray(4)
+    println("Utilizando forEach")
 
-    idades[0] = 25
-    idades[1] = 19
-    idades[2] = 33
-    idades[3] = 20
+    var menorIdade = Int.MAX_VALUE
+
+    idades.forEach { idade ->
+        if (idade < menorIdade) menorIdade = idade
+    }
+
+    println("Menor Idade: $menorIdade")
 }
